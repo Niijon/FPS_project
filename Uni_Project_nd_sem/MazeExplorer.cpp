009eb9c8332a2ab9aec5449751aff3e19ee1e6dd
Niijon <niijon6@gmail.com>
2020-05-29 14:44:24 +0200

    Update MazeExplorer.cpp

--- Uni_Project_nd_sem/MazeExplorer.cpp
+++ Uni_Project_nd_sem/MazeExplorer.cpp
@@ -2,6 +2,29 @@
 
 #include "MazeExplorer.h"
 
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+#include <GL/glu.h>
+
+
+void SetupLights(){
+    GLfloat light_position[] = { 2.0, 0.0, 2.0, 1.0 };
+    GLfloat light_ambient[] = { 0.0, 0.0, 0.0, 1.0 };
+    GLfloat light_diffuse[] = { 0.8, 0.8, 0.8, 1.0 };
+    GLfloat light_specular[] = { 0.0, 0.0, 0.0, 1.0 };
+
+    glLightfv( GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv( GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv( GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv( GL_LIGHT0, GL_SPECULAR, light_specular);
+    glEnable( GL_LIGHTING );
+    glEnable( GL_LIGHT0 );
+
+    GLfloat global_ambient[] = {0.3, 0.3, 0.3, 0.1};
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, global_ambient);
+
+    glEnable(GL_NORMALIZE) ;
+}
 
 void GameLoop(){
 
