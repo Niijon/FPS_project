30f3c3764a373a3cf4b545c0aebce15d1b97d382
Niijon <niijon6@gmail.com>
2020-05-29 14:44:26 +0200

    Update MapBlocks.h

--- Uni_Project_nd_sem/MapBlocks.h
+++ Uni_Project_nd_sem/MapBlocks.h
@@ -5,19 +5,21 @@ class MapStructure{
 
 public:
     virtual bool CollisionDetection()=0;
+    virtual void Draw(double size)=0;
 };
 
 class Floor : public MapStructure{
 
 
 public:
-
+    void Draw(double size) override;
 };
 
 class Wall : public MapStructure{
 
 
 public:
+    void Draw(double size) override;
 
 };
 
