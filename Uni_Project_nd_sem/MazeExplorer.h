8b83523d6302fc05270340e0b521e8bc2150d923
Niijon <niijon6@gmail.com>
2020-06-01 15:39:31 +0200

    Update MazeExplorer.h

--- Uni_Project_nd_sem/MazeExplorer.h
+++ Uni_Project_nd_sem/MazeExplorer.h
@@ -1,6 +1,18 @@
 #ifndef MAZEEXPLORER_H
 #define MAZEEXPLORER_H
 
+struct vec3d{
+    float x;
+    float y;
+    float z;
+};
+
+struct vec2d{
+  float x;
+  float y;
+};
+
+
 void GameLoop();
 void SetupLights();
 void SetView();
