45d67a446f2b0895228aa20f45a7d5ed8bfddba8
Niijon <niijon6@gmail.com>
2020-06-04 14:46:18 +0200

    Update CameraComponent.h

--- Uni_Project_nd_sem/CameraComponent.h
+++ Uni_Project_nd_sem/CameraComponent.h
@@ -1,10 +1,7 @@
 #ifndef CAMERACOMPONENT_H
 #define CAMERACOMPONENT_H
 
-#include <MazeExplorer.h>
-
-#include <GL/glu.h>
-
+#include "MazeExplorer.h"
 
 class Camera{
 private:
@@ -12,12 +9,14 @@ private:
     vec3d CameraTarget;
     vec3d CameraUp;
     float CameraSpeed;
+    float CameraAngle;
 
 public:
     Camera();
-    Camera(vec3d,vec3d,vec3d,float);
+    Camera(vec3d,vec3d,vec3d,float,float);
     void SetView();
     void MoveCam(vec2d,float);
+    void RotateCam(vec3d,float);
 
 };
 
