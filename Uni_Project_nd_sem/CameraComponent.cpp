a8e788262a729c0500257ea6ba89aa7e1e49a83c
Niijon <niijon6@gmail.com>
2020-06-04 14:46:58 +0200

    Update CameraComponent.cpp
    Figuring out camera movement and working on math operations inside of program matrix classes etc.

--- Uni_Project_nd_sem/CameraComponent.cpp
+++ Uni_Project_nd_sem/CameraComponent.cpp
@@ -1,34 +1,51 @@
 
-
 #include "CameraComponent.h"
 
-Camera::Camera(vec3d CameraPos_, vec3d CameraTarget_, vec3d CameraUp_, float CameraSpeed_){
+#include <GL/glu.h>
+
+
+Camera::Camera(vec3d CameraPos_, vec3d CameraTarget_, vec3d CameraUp_, float CameraSpeed_, float CameraAngle_){
     CameraPos = CameraPos_;
     CameraTarget = CameraTarget_;
     CameraUp = CameraUp_;
     CameraSpeed = CameraSpeed_;
+    CameraAngle = CameraAngle_;
 }
-
 
 Camera::Camera(){
     CameraPos.x = 10.0;
     CameraPos.y = 1.0;
-    CameraPos.z = 10.0;
-    CameraTarget.x = 0.0;
-    CameraTarget.y = 0.0;
-    CameraTarget.z = 0.0;
-    CameraUp.x = 0.0;
-    CameraUp.y = 1.0;
-    CameraUp.z = 0.0;
+    CameraPos.z = -60.0;
+    CameraTarget.x = 0.0f;
+    CameraTarget.y = 0.0f;
+    CameraTarget.z = -1.0f;
+    CameraUp.x = 0.0f;
+    CameraUp.y = 1.0f;
+    CameraUp.z = 0.0f;
+    CameraSpeed = 15.0f;
+    CameraAngle = 10.0f;
 }
 
+
 void Camera::SetView(){
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
     gluLookAt(CameraPos.x, CameraPos.y, CameraPos.z,
               CameraTarget.x, CameraTarget.y, CameraTarget.z,
               CameraUp.x, CameraUp.y,CameraUp.z);
+
+
 }
 
 
-void Camera::MoveCam(vec2d Direction_){
+void Camera::MoveCam(vec2d Direction_,float Del_){
+    CameraPos.x += Direction_.x*Del_*CameraSpeed;
+    CameraPos.z += Direction_.z*Del_*CameraSpeed;
+    CameraTarget.x += Direction_.x*Del_*CameraSpeed;
+    CameraTarget.z += Direction_.z*Del_*CameraSpeed;
+}
+
+
+void Camera::RotateCam(vec3d angles,float Del_){
 
 }
