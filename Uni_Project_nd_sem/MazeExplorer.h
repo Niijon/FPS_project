0727cc2f1f62588e6aede8ba9418847ecbe9a669
Niijon <niijon6@gmail.com>
2020-06-08 14:17:51 +0200

    Update MazeExplorer.h

--- Uni_Project_nd_sem/MazeExplorer.h
+++ Uni_Project_nd_sem/MazeExplorer.h
@@ -12,33 +12,6 @@ struct vec2d{
   float z;
 };
 
-class mat3{
-private:
-    float x1;
-    float x2;
-    float x3;
-    float x4;
-    float x5;
-    float x6;
-    float x7;
-    float x8;
-    float x9;
-    vec3d GetColumn1();
-    vec3d GetColumn2();
-    vec3d GetColumn3();
-    vec3d GetRow1();
-    vec3d GetRow2();
-    vec3d GetRow3();
-
-public:
-    mat3(float x1_, float x2_, float x3_, float x4_, float x5_, float x6_, float x7_, float x8_, float x9_);
-    mat3(vec3d,vec3d,vec3d);
-
-
-
-    mat3 MultiplyMatrix(mat3);
-};
-
 vec3d MultiplyByConstant(vec3d,float);
 
 
