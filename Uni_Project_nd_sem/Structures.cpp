ecfec09c7483a8119e36e90dfe97d1fbd21a903e
Niijon <niijon6@gmail.com>
2020-06-18 16:18:55 +0200

    Create Structures.cpp

---
+++ Uni_Project_nd_sem/Structures.cpp
@@ -0,0 +1,16 @@
+#include "Structures.h"
+
+Vec2d::Vec2d(int _x, int _y){
+    x=_x;
+    y=_y;
+}
+
+Vec2d::Vec2d(){}
+
+
+Vec2Param::Vec2Param(int _x, int _y, int _width, int _length){
+    x = _x;
+    y = _y;
+    width = _width;
+    length = _length;
+}
