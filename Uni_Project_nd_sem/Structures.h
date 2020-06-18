abbc4ad15fdaa73c40f06b677ae4d0e81717f380
Niijon <niijon6@gmail.com>
2020-06-18 16:18:57 +0200

    Create Structures.h

---
+++ Uni_Project_nd_sem/Structures.h
@@ -0,0 +1,47 @@
+#ifndef HERO_H
+#define HERO_H
+
+struct vec3d{
+    float x;
+    float y;
+    float z;
+};
+
+struct vec2d{
+  float x;
+  float z;
+};
+
+
+class Vec2d{
+public:
+    int x;
+    int y;
+
+    Vec2d();
+    Vec2d(int,int);
+};
+
+
+class Vec2Param{
+public:
+    int x;
+    int y;
+    int width;
+    int length;
+
+    Vec2Param(int,int,int,int);
+};
+
+struct Cell{
+    bool visited;
+    bool upw;
+    bool downw;
+    bool leftw;
+    bool rightw;
+    int data;
+};
+
+
+
+#endif // HERO_H
