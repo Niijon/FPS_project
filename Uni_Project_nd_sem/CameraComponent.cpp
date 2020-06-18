502662164a3fb3a838deeb61788d6c69feb4f201
Niijon <niijon6@gmail.com>
2020-06-18 19:00:08 +0200

    Update CameraComponent.cpp

--- Uni_Project_nd_sem/CameraComponent.cpp
+++ Uni_Project_nd_sem/CameraComponent.cpp
@@ -30,6 +30,8 @@ Camera::Camera(){
     CameraSpeed = 0.05f;
     Pitch = 0.0f;
     Yaw = 0.0f;
+    StepX = 0;
+    StepZ = 0;
 }
 
 
@@ -39,29 +41,37 @@ void Camera::SetView(){
 void Camera::MoveForward(float Del_){
     StepZ += -(Del_* CameraSpeed * sin((Yaw+90)*TO_RADIANS));
     StepX += Del_* CameraSpeed * cos((Yaw+90)*TO_RADIANS);
+    std::cout << CameraPos.x << " " << CameraPos.z << std::endl;
 }
 
 void Camera::MoveBackwards(float Del_){
     StepX += Del_* CameraSpeed * cos((Yaw+270)*TO_RADIANS);
     StepZ += -(Del_* CameraSpeed * sin((Yaw+270)*TO_RADIANS));
+    std::cout << CameraPos.x << " " << CameraPos.z << std::endl;
 }
 
 
 void Camera::StrafeRight(float Del_){
     StepX += Del_* CameraSpeed * cos((Yaw)*TO_RADIANS);
     StepZ += -(Del_* CameraSpeed * sin((Yaw)*TO_RADIANS));
+    std::cout << CameraPos.x << " " << CameraPos.z << std::endl;
 }
 
 void Camera::StrafeLeft(float Del_){
     StepX += Del_* CameraSpeed * cos((Yaw+180)*TO_RADIANS);
     StepZ += -(Del_* CameraSpeed * sin((Yaw+180)*TO_RADIANS));
+    std::cout << CameraPos.x << " " << CameraPos.z << std::endl;
 }
 
 void Camera::UpdateCam(){
+//    CameraPos.x += StepX;
+//    CameraPos.z += StepZ;
     glLoadIdentity();
     glRotatef(-Pitch, 1.0, 0.0, 0.0);
     glRotatef(-Yaw, 0.0, 1.0, 0.0);
     glTranslatef(-CameraPos.x, 0.0, -CameraPos.z);
+    StepX=0;
+    StepZ=0;
 }
 
 
@@ -90,12 +100,52 @@ void Camera::SetCamPos(vec2d camPos){
 
 
 
-bool Camera::CanMove(std::vector<std::vector<Cell>> cellV){
+void Camera::CanMove(std::vector<std::vector<Cell>> cellV){
     int Xid = (int)CameraPos.x / 1;
     int Zid = (int)CameraPos.z / 1;
 
+    if((CameraPos.x+StepX)>=(-0.11+double(Xid+1)) && (cellV[Xid+1][Zid].data == 1)){
+        StepX = 0;
+        std::cout << Xid << " " << Zid << " " << CameraPos.x << " " << CameraPos.z << " RIGHT" << std::endl;
+    }
+
+    if((CameraPos.x+StepX)<=(-0.11+double(Xid)) && cellV[Xid-1][Zid].data == 1){
+        StepX = 0;
+        std::cout << Xid << " " << Zid << " " << CameraPos.x << " " << CameraPos.z << " LEFT" << std::endl;
+    }
+
+    if((CameraPos.z+StepZ)>=(-0.11+double(Zid+1)) && cellV[Xid][Zid+1].data == 1){
+        StepZ = 0;
+        std::cout << Xid << " " << Zid << " " << CameraPos.x << " " << CameraPos.z << "DOWN" << std::endl;
+    }
+
+    if((CameraPos.z+StepZ)<=(-0.11+double(Zid)) && cellV[Xid][Zid-1].data == 1){
+        StepZ = 0;
+        std::cout << Xid << " " << Zid << " " << CameraPos.x << " " << CameraPos.z << "UP" << std::endl;
+    }
+
+
+
+
+    double size=cellV.size();
 
     CameraPos.x += StepX;
     CameraPos.z += StepZ;
+
+    if(CameraPos.x <=1.0 ){
+        CameraPos.x = 1.05;
+    }
+
+    else if(CameraPos.x >=size-1){
+        CameraPos.x = size-0.05;
+    }
+
+    if(CameraPos.z<= 1.0){
+        CameraPos.z = 1.05;
+    }
+
+    else if(CameraPos.z >=size-1){
+        CameraPos.z = size-0.05;
+    }
 }
 
