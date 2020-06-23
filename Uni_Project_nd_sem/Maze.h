4f997de084bf91317a3e0ef1d23fdea1bbe0f546
Niijon <niijon6@gmail.com>
2020-06-23 19:53:41 +0200

    Update Maze.h

--- Uni_Project_nd_sem/Maze.h
+++ Uni_Project_nd_sem/Maze.h
@@ -2,21 +2,30 @@
 #include <vector>
 
 #include "MapBlocks.h"
+#include "ChasingCube.h"
+
+
 
 class Maze {
  private:
   int size;
   int totalCells;
   int visitedCells;
-  std::vector<Cell> CellsVector;
+  std::vector<std::vector<Cell>> CellsVector;
+  std::vector<std::vector<ACell>> ACellVector;
   Vec2d startPos;
   Vec2d goalPos;
 
+  bool ReachedGoal(float,float);
+
 
  public:
-  Maze(int size);
+  Maze(const int &size);
   std::vector<std::vector<Cell>> generateMaze();
   void convertMaze();
   Vec2d getStartPos();
   Vec2d getGoalPos();
+
+  void AStar();
+  ChasingCube SpawnChasingCube();
 };
