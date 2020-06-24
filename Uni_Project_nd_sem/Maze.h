fad88768741490196cb5b50285070ca38a6e0f38
Niijon <niijon6@gmail.com>
2020-06-24 18:48:09 +0200

    Update Maze.h

--- Uni_Project_nd_sem/Maze.h
+++ Uni_Project_nd_sem/Maze.h
@@ -11,10 +11,13 @@ class Maze {
   int size;
   int totalCells;
   int visitedCells;
+  int ChasersTotal;
   std::vector<std::vector<Cell>> CellsVector;
   std::vector<std::vector<ACell>> ACellVector;
+  std::vector<Cell> MovingSpaceV;
   Vec2d startPos;
   Vec2d goalPos;
+  Vec2d Pos;
 
   bool ReachedGoal(float,float);
 
@@ -24,8 +27,9 @@ class Maze {
   std::vector<std::vector<Cell>> generateMaze();
   void convertMaze();
   Vec2d getStartPos();
+  void setPos(Vec2d);
   Vec2d getGoalPos();
 
-  void AStar();
+  void AStar(double&,std::vector<ChasingCube>&);
   ChasingCube SpawnChasingCube();
 };
